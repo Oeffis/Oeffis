590e22a7738c0d8d3a82f33e1806f0821741ee95
Dominik Krakau <dominik.krakau@outlook.de>
2023-05-29 18:50:36 +0200

    create typescript client from backend swagger json

---
+++ frontend/src/api/models/AppHttpException.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type AppHttpException = {
+    /**
+     * Http status code
+     */
+    statusCode: number;
+    /**
+     * Error message
+     */
+    message: string;
+};
---
+++ frontend/src/api/models/User.ts
@@ -0,0 +1,18 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type User = {
+    /**
+     * Name of a User
+     */
+    name: string;
+    /**
+     * Age of a User
+     */
+    age: number;
+    /**
+     * City of a User
+     */
+    city: string;
+};
