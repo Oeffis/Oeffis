4946336fe67eec7f3dc50ef7c9f191d3f58f9b85
Dominik Krakau <dominik.krakau@outlook.de>
2023-05-29 18:44:17 +0200

    implementation of hello world endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { HelloWorld } from './helloworld.entity';
 
-@Controller()
+@Controller('app')
+@ApiTags('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('helloworld')
+  @ApiOperation({ summary: 'Returns "Hello World!"' })
+  @ApiOkResponse({ description: 'Returns a "Hello World!" message.', type: [HelloWorld] })
   getHello(): string {
     return this.appService.getHello();
   }
