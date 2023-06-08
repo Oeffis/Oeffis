b65e12d60a7e19e21605f8a3d51088340563e869
Jan Jakobus <jan.jakobus@studmail.w-hs.de>
2023-06-08 14:23:37 +0200

    #35 Creates journeys module based on HAFAS
    - adds extern module 'hafas-client' using lazy injection
    with dynamic import as workaround
    - implements journeys service (based on HAFAS)
    - defines endpoints for searching locations, planning
    journeys and refreshing journeys

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app/controller/app.controller";
 import { AppService } from "./app/service/app.service";
 import { UsersModule } from "./users/users.module";
+import { JourneyModule } from "./journey/journey.module";
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JourneyModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/symbols.ts
@@ -0,0 +1,2 @@
+export const HAFAS_CLIENT = Symbol("HAFAS_CLIENT");
+export const HAFAS_LANGUAGE = "de";
