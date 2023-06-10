17741fd0728b618731e1fefbaf18dafbd19c791a
Jan Jakobus <jan.jakobus@studmail.w-hs.de>
2023-06-10 15:02:01 +0200

    #35 Adds hafas-client testwise

---
+++ backend/src/journey/entities/station.entity.ts
@@ -0,0 +1,15 @@
+import {ApiProperty} from "@nestjs/swagger";
+
+/**
+ * Represents one waypoint of a journey.
+ */
+export class Station {
+
+  @ApiProperty({
+    description: 'Name of a station.',
+    example: 'Wuppertal Hbf',
+    required: true,
+  })
+  name: string;
+
+}
--- backend/src/journey/journey.controller.ts
+++ backend/src/journey/journey.controller.ts
@@ -1,6 +1,14 @@
 import { Controller, Get, Inject } from "@nestjs/common";
 import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { JourneyService } from "./services/journey.service";
+import { createClient } from 'hafas-client';
+import { profile as dbProfile } from 'hafas-client/p/db/index.js';
+
+// Adapt this to your project! createClient() won't work with this string.
+const userAgent = 'link-to-your-project-or-email';
+
+// create a client with the Deutsche Bahn profile
+const client = createClient(dbProfile, userAgent);
 
 /**
  * Endpoint for request regarding planning a journey. 
@@ -19,7 +27,14 @@ export class JourneyController {
   @Get('get/test')
   @ApiOperation({ summary: 'Test method.' })
   @ApiOkResponse({ description: 'Worked.' })
-  respond() {
+  async respond() {
+
+    // Berlin Jungfernheide to München Hbf
+    const { journeys } = await client.journeys('8011167', '8000261', {
+      results: 1,
+    });
+    console.log(journeys[0]);
+
     return 'Just some response.';
   }
 
