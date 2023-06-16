df0aed46f63c3f672f81c1eec3a14ab1e9393f30
Daniel Kojok <65492644+Escowei98@users.noreply.github.com>
2023-06-16 15:25:46 +0200

    eslint configuration (#59)
    * eslint configuration
    
    * fixed dependency problems
    
    * eslint file in root project
    
    * package lock file
    
    * changes in base eslint file
    
    * differentiate eslint-rules clearer between frontend and backend
    
    * added self-closing-component rule
    
    * no debugger rule and --fix run for frontend
    
    * lint fixes
    
    * lint fixes
    
    * changed lint rules
    
    * turned on "no-explicit-any" rule

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -6,7 +6,7 @@ import {
   Param,
   Patch,
   Post,
-} from '@nestjs/common';
+} from "@nestjs/common";
 import {
   ApiCreatedResponse,
   ApiNotFoundResponse,
@@ -14,66 +14,66 @@ import {
   ApiOperation,
   ApiParam,
   ApiTags,
-} from '@nestjs/swagger';
-import { AppHttpException } from '../app.http.exception';
-import { User } from './entities/user.entity';
-import { UserService } from './user.service';
+} from "@nestjs/swagger";
+import { AppHttpException } from "../app.http.exception";
+import { User } from "./entities/user.entity";
+import { UserService } from "./user.service";
 
-@Controller('user')
-@ApiTags('user')
+@Controller("user")
+@ApiTags("user")
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
-  @ApiOperation({ summary: 'Create a User' })
-  @ApiCreatedResponse({ description: 'Create a User.', type: User })
-  create(@Body() createUser: User) {
+  @ApiOperation({ summary: "Create a User" })
+  @ApiCreatedResponse({ description: "Create a User.", type: User })
+  create(@Body() createUser: User): unknown {
     return this.userService.create(createUser);
   }
 
-  @Get('get/all')
-  @ApiOperation({ summary: 'Return all Users' })
+  @Get("get/all")
+  @ApiOperation({ summary: "Return all Users" })
   @ApiOkResponse({
-    description: 'Return all Users as JSON array.',
+    description: "Return all Users as JSON array.",
     type: [User],
   })
-  findAll() {
+  findAll(): unknown {
     return this.userService.findAll();
   }
 
-  @Get('get/:index')
-  @ApiOperation({ summary: 'Return a User by index' })
-  @ApiOkResponse({ description: 'Return a User by index.', type: User })
+  @Get("get/:index")
+  @ApiOperation({ summary: "Return a User by index" })
+  @ApiOkResponse({ description: "Return a User by index.", type: User })
   @ApiNotFoundResponse({
-    description: 'User not found.',
+    description: "User not found.",
     type: AppHttpException,
   })
-  @ApiParam({ description: 'Users index', name: 'index' })
-  findOne(@Param('index') index: string) {
+  @ApiParam({ description: "Users index", name: "index" })
+  findOne(@Param("index") index: string): unknown {
     return this.userService.findOne(+index);
   }
 
-  @Patch('update/:index')
-  @ApiOperation({ summary: 'Update a User by index' })
-  @ApiCreatedResponse({ description: 'Update a User by index.', type: User })
+  @Patch("update/:index")
+  @ApiOperation({ summary: "Update a User by index" })
+  @ApiCreatedResponse({ description: "Update a User by index.", type: User })
   @ApiNotFoundResponse({
-    description: 'User not found.',
+    description: "User not found.",
     type: AppHttpException,
   })
-  @ApiParam({ description: 'Users index', name: 'index' })
-  update(@Param('index') index: string, @Body() updateUser: User) {
+  @ApiParam({ description: "Users index", name: "index" })
+  update(@Param("index") index: string, @Body() updateUser: User): unknown {
     return this.userService.update(+index, updateUser);
   }
 
-  @Delete('delete/:index')
-  @ApiOperation({ summary: 'Delete a User by index' })
-  @ApiOkResponse({ description: 'Delete a User by index.', type: User })
+  @Delete("delete/:index")
+  @ApiOperation({ summary: "Delete a User by index" })
+  @ApiOkResponse({ description: "Delete a User by index.", type: User })
   @ApiNotFoundResponse({
-    description: 'User not found.',
+    description: "User not found.",
     type: AppHttpException,
   })
-  @ApiParam({ description: 'Users index', name: 'index' })
-  remove(@Param('index') index: string) {
+  @ApiParam({ description: "Users index", name: "index" })
+  remove(@Param("index") index: string): unknown {
     return this.userService.remove(+index);
   }
 }
