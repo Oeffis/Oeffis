309a2141190a5661257f401001d067e8eeedcac1
Dominik Krakau <dominik.krakau@outlook.de>
2023-06-19 19:35:59 +0200

    Add UpdateUser function to UserContainer component

--- frontend/src/components/UserContainer.tsx
+++ frontend/src/components/UserContainer.tsx
@@ -12,6 +12,11 @@ const UserContianer: React.FC<ContainerProps> = () => {
     const [createUserAge, setCreateUserAge] = useState<number>(0);
     const [createUserCity, setCreateUserCity] = useState<string>("");
 
+    const [updateUserIndex, setUpdateUserIndex] = useState<string>("");
+    const [updateUserName, setUpdateUserName] = useState<string>("");
+    const [updateUserAge, setUpdateUserAge] = useState<number>(0);
+    const [updateUserCity, setUpdateUserCity] = useState<string>("");
+
     const fetchHelloWorld = async () => {
         setHelloWorld(await AppService.appControllerGetHello());
     }
@@ -24,6 +29,25 @@ const UserContianer: React.FC<ContainerProps> = () => {
         }
         await UserService.userControllerCreate(user);
     }
+
+    const loadUser = async (index: string) => {
+        setUpdateUserIndex(index);
+        let user = await UserService.userControllerFindOne(index);
+        console.log(user);
+        setUpdateUserName(user.name);
+        setUpdateUserAge(user.age);
+        setUpdateUserCity(user.city);
+    }
+
+    const updateUser = async () => {
+        let user = {
+            name: updateUserName,
+            age: updateUserAge,
+            city: updateUserCity
+        }
+        await UserService.userControllerUpdate(updateUserIndex, user);
+    }
+
 
     return (
         <div className="UserContianer">
@@ -58,6 +82,29 @@ const UserContianer: React.FC<ContainerProps> = () => {
                         </IonItem>
                         <IonItem>
                             <IonButton onClick={createUser}>CREATE USER</IonButton>
+                        </IonItem>
+                    </IonList>
+                </IonCard>
+
+                <IonCard>
+                    <IonCardHeader>
+                        <IonCardTitle>Update User</IonCardTitle>
+                    </IonCardHeader>
+                    <IonList>
+                        <IonItem>
+                            <IonInput onIonChange={e => loadUser(e.detail.value!)} label="User index" placeholder="Enter user index"></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput onIonChange={e => setUpdateUserName(e.detail.value!)} id="uun" value={updateUserName} label="User name" placeholder="Enter user name"></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput onIonChange={e => setUpdateUserAge(Number.parseInt(e.detail.value!))} id="uua" value={updateUserAge} label="Age" type="number" placeholder="0"></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonInput onIonChange={e => setUpdateUserCity(e.detail.value!)} id="uuc" value={updateUserCity} label="City" placeholder="Enter city name"></IonInput>
+                        </IonItem>
+                        <IonItem>
+                            <IonButton onClick={updateUser}>UPDATE USER</IonButton>
                         </IonItem>
                     </IonList>
                 </IonCard>
