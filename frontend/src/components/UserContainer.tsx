cf880db751439e8939ca9b0bb044ecbc8e4cb1a2
Dominik Krakau <dominik.krakau@outlook.de>
2023-06-19 19:25:48 +0200

    Add HelloWorld function to UserContainer component

--- frontend/src/components/UserContainer.tsx
+++ frontend/src/components/UserContainer.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { AppService, HelloWorld } from "../api";
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonList } from "@ionic/react";
+
 interface ContainerProps { }
 
 const UserContianer: React.FC<ContainerProps> = () => {
-    return(
+
+    const [helloWorld, setHelloWorld] = useState<Array<HelloWorld>>();
+
+    const fetchHelloWorld = async () => {
+        setHelloWorld(await AppService.appControllerGetHello());
+    }
+
+    return (
         <div className="UserContianer">
-            
+            <IonList>
+
+                <IonCard>
+                    <IonCardHeader>
+                        <IonCardTitle>Hello World!</IonCardTitle>
+                        <IonCardSubtitle>Displays a "Hello World!" message received from backend.</IonCardSubtitle>
+                    </IonCardHeader>
+                    <IonCardContent className="message">Response: {helloWorld?.toString()}</IonCardContent>
+                    <IonList>
+                        <IonItem>
+                            <IonButton onClick={fetchHelloWorld}>SEND REQUEST</IonButton>
+                        </IonItem>
+                    </IonList>
+                </IonCard>
+
+            </IonList>
         </div>
     );
 
