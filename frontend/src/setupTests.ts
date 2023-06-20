8f111d7839f7e90ddeaa4e75b5db43ee88ad4b37
Tim Vahlbrock <tim@vahlbrock.de>
2023-06-20 14:56:00 +0200

    activate linter in frontend, fix linting errors

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -2,18 +2,23 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
 
 // Mock matchmedia
-window.matchMedia = window.matchMedia || function () {
+window.matchMedia = window.matchMedia || function (): MediaQueryList {
   return {
     matches: false,
-    addListener: function () { },
-    removeListener: function () { }
-  };
+    addListener: function (): void {
+      // do nothing
+    },
+    removeListener: function (): void {
+      // do nothing
+    }
+  } as Partial<MediaQueryList> as MediaQueryList;
 };
 
 // Disable rendering of certain components which have issues running in jsdom
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 window.Ionic = {
   config: {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vitest/config'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
   }
-})
+});
