f72a501fd1d3287f1afa92dd7d134dd61f79afd0
Dominik Krakau <dominik.krakau@outlook.de>
2023-06-20 13:22:40 +0200

    Removed unneccessary import paths

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
-import { UsersService } from "users/users.service";
+import { UsersService } from "./users.service";
 
 @Module({
   controllers: [UsersController],
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
-import { User } from "users/entities/user.entity";
+import { User } from "./entities/user.entity";
 
 const users: User[] = [];
 
