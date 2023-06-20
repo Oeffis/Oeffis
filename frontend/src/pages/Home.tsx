2c96eee4dcdd6b9e72137d2c8e02c1906ad9cb42
Tim Vahlbrock <tim@vahlbrock.de>
2023-06-20 11:09:25 +0200

    show map

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
-import UserContainer from "../components/UserContainer";
+import { IonContent, IonPage } from "@ionic/react";
+import LeafletMapContainer from "../components/LeafletMapContainer";
 import "./Home.css";
 
 const Home: React.FC = () => (
   <IonPage>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>Blank</IonTitle>
-      </IonToolbar>
-    </IonHeader>
     <IonContent fullscreen>
-      <IonHeader collapse="condense">
-        <IonToolbar>
-          <IonTitle size="large">Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <ExploreContainer />
-      <UserContainer />
+      <LeafletMapContainer />
     </IonContent>
   </IonPage>
 );
