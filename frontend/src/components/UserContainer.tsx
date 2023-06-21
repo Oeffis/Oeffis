6a813b80415d59ee7fb9857146db2db9603da023
Tim Vahlbrock <tim@vahlbrock.de>
2023-06-21 18:03:24 +0200

    fix linting errors

--- frontend/src/components/UserContainer.tsx
+++ frontend/src/components/UserContainer.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from "react";
-import { AppService, HelloWorld, User, UsersService } from "../api";
 import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonItem, IonLabel, IonList } from "@ionic/react";
+import { JSX, useState } from "react";
+import { AppService, HelloWorld, User, UsersService } from "../api";
 
-interface ContainerProps { }
-
-const UserContainer: React.FC<ContainerProps> = () => {
-
+const UserContainer: React.FC = () => {
     const [helloWorld, setHelloWorld] = useState<Array<HelloWorld>>();
     const [users, setUsers] = useState<Array<User>>([]);
 
@@ -23,80 +20,78 @@ const UserContainer: React.FC<ContainerProps> = () => {
 
     const [userInfo, setUserInfo] = useState<string>("");
 
-    const fetchHelloWorld = async () => {
+    const fetchHelloWorld = async (): Promise<void> => {
         setHelloWorld(await AppService.appControllerGetHello());
-    }
+    };
 
-    const createUser = async () => {
-        let user = {
+    const createUser = async (): Promise<void> => {
+        const user = {
             name: createUserName,
             age: createUserAge,
             city: createUserCity
-        }
+        };
         await UsersService.usersControllerCreate(user);
-    }
+    };
 
-    const loadUser = async (index: string) => {
+    const loadUser = async (index: string): Promise<void> => {
         setUpdateUserIndex(index);
-        let user = await UsersService.usersControllerFindOne(index);
+        const user = await UsersService.usersControllerFindOne(index);
         console.log(user);
         setUpdateUserName(user.name);
         setUpdateUserAge(user.age);
         setUpdateUserCity(user.city);
-    }
+    };
 
-    const updateUser = async () => {
-        let user = {
+    const updateUser = async (): Promise<void> => {
+        const user = {
             name: updateUserName,
             age: updateUserAge,
             city: updateUserCity
-        }
+        };
         await UsersService.usersControllerUpdate(updateUserIndex, user);
-    }
+    };
 
-    const deleteUser = async () => {
-        console.log(deleteUserByIndex)
+    const deleteUser = async (): Promise<void> => {
+        console.log(deleteUserByIndex);
         await UsersService.usersControllerRemove(deleteUserByIndex);
-    }
+    };
 
-    const getUser = async () => {
-        let user = await UsersService.usersControllerFindOne(getUserByIndex);
+    const getUser = async (): Promise<void> => {
+        const user = await UsersService.usersControllerFindOne(getUserByIndex);
         setUserInfo(JSON.stringify(user));
-    }
+    };
 
-    const fetchUsers = async () => {
+    const fetchUsers = async (): Promise<void> => {
         setUsers(await UsersService.usersControllerFindAll());
         console.log(users);
-    }
+    };
 
-    const getAllUsers = () => {
-        return (
-            <IonList>
-                <IonItem>
-                    <IonLabel>index</IonLabel>
-                    <IonLabel>name</IonLabel>
-                    <IonLabel>age</IonLabel>
-                    <IonLabel>city</IonLabel>
+    const getAllUsers = (): JSX.Element => (
+        <IonList>
+            <IonItem>
+                <IonLabel>index</IonLabel>
+                <IonLabel>name</IonLabel>
+                <IonLabel>age</IonLabel>
+                <IonLabel>city</IonLabel>
+            </IonItem>
+            {users.map((user, index) => (
+                <IonItem key={index}>
+                    <IonLabel>
+                        {index}
+                    </IonLabel>
+                    <IonLabel>
+                        {user.name}
+                    </IonLabel>
+                    <IonLabel>
+                        {user.age}
+                    </IonLabel>
+                    <IonLabel>
+                        {user.city}
+                    </IonLabel>
                 </IonItem>
-                {users.map((user, index) => (
-                    <IonItem key={index}>
-                        <IonLabel>
-                            {index}
-                        </IonLabel>
-                        <IonLabel>
-                            {user.name}
-                        </IonLabel>
-                        <IonLabel>
-                            {user.age}
-                        </IonLabel>
-                        <IonLabel>
-                            {user.city}
-                        </IonLabel>
-                    </IonItem>
-                ))}
-            </IonList>
-        );
-    }
+            ))}
+        </IonList>
+    );
 
     /* useEffect(() => {
         fetchUsers();
@@ -125,13 +120,16 @@ const UserContainer: React.FC<ContainerProps> = () => {
                     </IonCardHeader>
                     <IonList>
                         <IonItem>
-                            <IonInput onIonChange={e => setCreateUserName(e.detail.value!)} label="User name" placeholder="Enter user name"></IonInput>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
+                            <IonInput onIonChange={e => setCreateUserName(e.detail.value!)} label="User name" placeholder="Enter user name" />
                         </IonItem>
                         <IonItem>
-                            <IonInput onIonChange={e => setCreateUserAge(Number.parseInt(e.detail.value!))} label="Age" type="number" placeholder="0"></IonInput>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
+                            <IonInput onIonChange={e => setCreateUserAge(Number.parseInt(e.detail.value!))} label="Age" type="number" placeholder="0" />
                         </IonItem>
                         <IonItem>
-                            <IonInput onIonChange={e => setCreateUserCity(e.detail.value!)} label="City" placeholder="Enter city name"></IonInput>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
+                            <IonInput onIonChange={e => setCreateUserCity(e.detail.value!)} label="City" placeholder="Enter city name" />
                         </IonItem>
                         <IonItem>
                             <IonButton onClick={createUser}>CREATE USER</IonButton>
@@ -145,18 +143,23 @@ const UserContainer: React.FC<ContainerProps> = () => {
                     </IonCardHeader>
                     <IonList>
                         <IonItem>
-                            <IonInput onIonChange={e => loadUser(e.detail.value!)} label="User index" placeholder="Enter user index"></IonInput>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
+                            <IonInput onIonChange={e => loadUser(e.detail.value!)} label="User index" placeholder="Enter user index" />
                         </IonItem>
                         <IonItem>
-                            <IonInput onIonChange={e => setUpdateUserName(e.detail.value!)} id="uun" value={updateUserName} label="User name" placeholder="Enter user name"></IonInput>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
+                            <IonInput onIonChange={e => setUpdateUserName(e.detail.value!)} id="uun" value={updateUserName} label="User name" placeholder="Enter user name" />
                         </IonItem>
                         <IonItem>
-                            <IonInput onIonChange={e => setUpdateUserAge(Number.parseInt(e.detail.value!))} id="uua" value={updateUserAge} label="Age" type="number" placeholder="0"></IonInput>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
+                            <IonInput onIonChange={e => setUpdateUserAge(Number.parseInt(e.detail.value!))} id="uua" value={updateUserAge} label="Age" type="number" placeholder="0" />
                         </IonItem>
                         <IonItem>
-                            <IonInput onIonChange={e => setUpdateUserCity(e.detail.value!)} id="uuc" value={updateUserCity} label="City" placeholder="Enter city name"></IonInput>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
+                            <IonInput onIonChange={e => setUpdateUserCity(e.detail.value!)} id="uuc" value={updateUserCity} label="City" placeholder="Enter city name" />
                         </IonItem>
                         <IonItem>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
                             <IonButton onClick={updateUser}>UPDATE USER</IonButton>
                         </IonItem>
                     </IonList>
@@ -168,7 +171,8 @@ const UserContainer: React.FC<ContainerProps> = () => {
                     </IonCardHeader>
                     <IonList>
                         <IonItem>
-                            <IonInput onIonChange={e => setDeleteUserByIndex(e.detail.value!)} label="Index" type="number" placeholder="0"></IonInput>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
+                            <IonInput onIonChange={e => setDeleteUserByIndex(e.detail.value!)} label="Index" type="number" placeholder="0" />
                         </IonItem>
                         <IonItem>
                             <IonButton onClick={deleteUser}>DELETE USER</IonButton>
@@ -183,7 +187,8 @@ const UserContainer: React.FC<ContainerProps> = () => {
                     </IonCardHeader>
                     <IonList>
                         <IonItem>
-                            <IonInput onIonChange={e => setGetUserByIndex(e.detail.value!)} label="Index" type="number" placeholder="0"></IonInput>
+                            {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/explicit-function-return-type */}
+                            <IonInput onIonChange={e => setGetUserByIndex(e.detail.value!)} label="Index" type="number" placeholder="0" />
                         </IonItem>
                         <IonItem>
                             <IonButton onClick={getUser}>GET USER</IonButton>
