12bb2a34c6b369b9233749f9e5aa2b5305f452ee
Tim Vahlbrock <tim@vahlbrock.de>
2023-06-22 14:18:36 +0200

    activate linter in frontend, fix linting errors (#153)
    * activate linter in frontend, fix linting errors
    
    * remove unexisting ./app from vscode settings
    
    * fix linting errors

--- frontend/src/components/ExploreContainer.tsx
+++ frontend/src/components/ExploreContainer.tsx
@@ -1,14 +1,10 @@
-import './ExploreContainer.css';
+import "./ExploreContainer.css";
 
-interface ContainerProps { }
-
-const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
-};
+const ExploreContainer: React.FC = () => (
+  <div className="container">
+    <strong>Ready to create an app?</strong>
+    <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+  </div>
+);
 
 export default ExploreContainer;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,27 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Home.css';
-import ExploreContainer from '../components/ExploreContainer';
-import UserContainer from '../components/UserContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import UserContainer from "../components/UserContainer";
+import "./Home.css";
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
+const Home: React.FC = () => (
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Blank</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent fullscreen>
+      <IonHeader collapse="condense">
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle size="large">Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-        <UserContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
+      <ExploreContainer />
+      <UserContainer />
+    </IonContent>
+  </IonPage>
+);
 
 export default Home;
