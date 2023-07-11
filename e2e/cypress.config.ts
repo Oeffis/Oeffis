8515091ea3442898bf8037a3de0965c92c9a9427
Tim Vahlbrock <tim@vahlbrock.de>
2023-07-11 23:47:17 +0200

    add support for gherkin files

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,10 +1,26 @@
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    supportFile: `support.ts`,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    supportFile: "support.ts",
+    specPattern: "**/*.feature",
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ): Promise<Cypress.PluginConfigOptions> {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
     },
   },
 });
