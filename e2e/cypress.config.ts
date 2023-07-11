9552b0d06e54d6915209815c873044204b5dceec
Tim Vahlbrock <tim@vahlbrock.de>
2023-07-11 23:12:57 +0200

    adapt cypress config to monorepo structure

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    supportFile: `support.ts`,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
