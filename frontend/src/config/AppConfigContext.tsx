30d7b629048cddd904d91a035e5e887aaece32b6
Tim Vahlbrock <tim@vahlbrock.de>
2023-07-28 10:31:43 +0200

    fix linting errors

--- frontend/src/config/AppConfigContext.tsx
+++ frontend/src/config/AppConfigContext.tsx
@@ -7,24 +7,24 @@ import { AppConfig } from "./appConfig";
 export const AppConfigContext = createContext<AppConfig>({} as AppConfig);
 
 async function fetcher<C>(url: string): Promise<C> {
-    const res = await fetch(url);
-    if (!res.ok) throw new Error(res.body?.toString() || "Failed to fetch");
-    return res.json();
+  const res = await fetch(url);
+  if (!res.ok) throw new Error(res.body?.toString() || "Failed to fetch");
+  return res.json();
 }
 
 export function AppConfigProvider(props: { children: React.ReactNode }): JSX.Element {
-    const { data, error } = useSWR<AppConfig>("/config/config.json", fetcher);
-    if (error) {
-        return <div>Failed to load config</div>;
-    }
-    if (!data) {
-        return <div>Loading config...</div>;
-    }
-    console.debug("appConfig", data);
-    OpenAPI.BASE = data.apiUrl;
-    return (
-        <AppConfigContext.Provider value={data}>
-            {props.children}
-        </AppConfigContext.Provider>
-    );
+  const { data, error } = useSWR<AppConfig>("/config/config.json", fetcher);
+  if (error) {
+    return <div>Failed to load config</div>;
+  }
+  if (!data) {
+    return <div>Loading config...</div>;
+  }
+  console.debug("appConfig", data);
+  OpenAPI.BASE = data.apiUrl;
+  return (
+    <AppConfigContext.Provider value={data}>
+      {props.children}
+    </AppConfigContext.Provider>
+  );
 }
