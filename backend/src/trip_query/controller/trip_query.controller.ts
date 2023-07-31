d68719a2d49b4d988c93a41b42cec132d7a24673
Tim Vahlbrock <tim@vahlbrock.de>
2023-07-31 18:19:38 +0200

    fix tests

--- backend/src/trip_query/controller/trip_query.controller.ts
+++ backend/src/trip_query/controller/trip_query.controller.ts
@@ -3,7 +3,7 @@ import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiTags } from "@ne
 import { BadRequest } from "app/entity/badrequest.entity";
 import { TripQueryRequestDto } from "trip_query/dto/tripQueryRequest";
 import { TripQueryResponseDto } from "trip_query/dto/tripQueryResponse";
-import { TripQueryService } from "trip_query/service/trip_query.service";
+import { TripQueryService } from "../service/trip_query.service";
 
 @Controller("trip_query")
 @ApiTags("trip_query")
