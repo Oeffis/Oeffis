31d1acfcf090d6b9ce59a723be52b229ce618b6d
da34470 <daniel.kojok@studmail.w-hs.de>
2023-07-31 12:41:44 +0200

    eslint problems fixed

--- frontend/src/components/JourneyListComponent.tsx
+++ frontend/src/components/JourneyListComponent.tsx
@@ -3,15 +3,15 @@ import { IJourney } from "../interfaces/IJourney.interface";
 import JourneyDetail from "./JourneyDetail";
 
 const JourneyListComponent: React.FC<{journeys: IJourney[]}> = (props: {journeys: IJourney[]}) => (
-        <div className="JourneyListComponent">
-            <IonList className="detail-card">
-                {props.journeys.map(
-                    journey => 
-                    <JourneyDetail journey={journey} />
-                    )}
-            </IonList>
-        </div>
-    );
+  <div className="JourneyListComponent">
+    <IonList className="detail-card">
+      {props.journeys.map(
+        journey => 
+          <JourneyDetail journey={journey} />
+      )}
+    </IonList>
+  </div>
+);
   
-  export default JourneyListComponent;
+export default JourneyListComponent;
   
--- frontend/src/components/JourneyStepComponent.tsx
+++ frontend/src/components/JourneyStepComponent.tsx
@@ -2,30 +2,30 @@ import "./JourneyStepComponent.css";
 import { IJourneyStep } from "../interfaces/IJourneyStep.interface";
 import { IonLabel } from "@ionic/react";
 
-export interface stationProps {step: IJourneyStep, first?: boolean, last?: boolean}
+export interface StationProps { step: IJourneyStep, first?: boolean, last?: boolean }
 
-const JourneyStepComponent: React.FC<stationProps> = (props: stationProps) => (
-    <div className="container">
-        <div className="left">
-            {props.step.arrivalTime}
-            <div className="timeline-container">
-            <div className="circle"/>
-            <div className="line"/>
-        </div>
-        </div>
-        <div className="step-info">
-            <IonLabel>
-                {props.step.stopName} "{props.step.stationName}"
-            </IonLabel>
-            <IonLabel>
-                Abfahrt: {props.step.startTime}
-            </IonLabel>
-            <IonLabel>
-                Ankunft: {props.step.arrivalTime}
-            </IonLabel>
-        </div>
+const JourneyStepComponent: React.FC<StationProps> = (props: StationProps) => (
+  <div className="container">
+    <div className="left">
+      {props.step.arrivalTime}
+      <div className="timeline-container">
+        <div className="circle" />
+        <div className="line" />
+      </div>
     </div>
-  );
-    
-  
-  export default JourneyStepComponent;
+    <div className="step-info">
+      <IonLabel>
+        {props.step.stopName} "{props.step.stationName}"
+      </IonLabel>
+      <IonLabel>
+        Abfahrt: {props.step.startTime}
+      </IonLabel>
+      <IonLabel>
+        Ankunft: {props.step.arrivalTime}
+      </IonLabel>
+    </div>
+  </div>
+);
+
+
+export default JourneyStepComponent;
