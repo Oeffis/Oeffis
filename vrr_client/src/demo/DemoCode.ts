0959088a4766cf4c868972ad151f3d0c3b18a78f
Tim Vahlbrock <tim@vahlbrock.de>
2023-07-31 17:30:55 +0200

    fix linting errors

--- vrr_client/src/demo/DemoCode.ts
+++ vrr_client/src/demo/DemoCode.ts
@@ -2,7 +2,7 @@ import {
   ServingLinesClient,
   VRR_TEST_API_BASE_URL,
   StopFinderClient,
-  TripRequestClient,
+  TripRequestClient
 } from "..";
 
 async function demoTripRequestClient(): Promise<void> {
@@ -11,7 +11,7 @@ async function demoTripRequestClient(): Promise<void> {
     destinationPointId:
       "coord:755620:5337741:MRCV:Stadtmitte (Düsseldorf), Jacobistraße 2:0",
     originPointId:
-      "poiID:2450:5513000:-1:Rathaus Buer:Gelsenkirchen:Rathaus Buer:ANY:POI:785651:5275982:MRCV:nrw",
+      "poiID:2450:5513000:-1:Rathaus Buer:Gelsenkirchen:Rathaus Buer:ANY:POI:785651:5275982:MRCV:nrw"
   });
 
   console.log("Planed trip:", result);
@@ -21,7 +21,7 @@ async function demoStopFinderClient(): Promise<void> {
   const stopFinderClient = new StopFinderClient(VRR_TEST_API_BASE_URL);
   const result = await stopFinderClient.findStopAtCoordinates({
     latitude: 51.231, // Breitengrad
-    longitude: 6.787835, // Längengrad
+    longitude: 6.787835 // Längengrad
   });
 
   console.log(
@@ -30,7 +30,7 @@ async function demoStopFinderClient(): Promise<void> {
   );
 
   const result2 = await stopFinderClient.findStopByName({
-    search: "Buer Rathaus",
+    search: "Buer Rathaus"
   });
 
   console.log(
@@ -45,11 +45,11 @@ async function demoServingLinesClient(): Promise<void> {
     // pointId: 'coord:755620:5337741:MRCV:Stadtmitte (Düsseldorf), Jacobistraße 2:0',
     // pointId: 'de:05111:18235',
     pointId:
-      "poiID:62546:5111000:-1:Rheinbahn KundenCenter Hauptbahnhof:Düsseldorf:Rheinbahn KundenCenter Hauptbahnhof:ANY:POI:756112:5339515:MRCV:nrw",
+      "poiID:62546:5111000:-1:Rheinbahn KundenCenter Hauptbahnhof:Düsseldorf:Rheinbahn KundenCenter Hauptbahnhof:ANY:POI:756112:5339515:MRCV:nrw"
   });
 
   const result2 = await servingLinesClient.findServingLinesByLineName({
-    search: "301",
+    search: "301"
   });
 
   console.log("Result:", result);
