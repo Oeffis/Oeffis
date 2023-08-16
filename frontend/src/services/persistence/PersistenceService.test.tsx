7d089ba785ab99cc5b2f5e5be348500ce3a7b898
Paul Heidenreich <paul@paul-h.de>
2023-08-16 10:39:56 +0200

    Add service to provide persistence via localstorage api
    Closes #90

---
+++ frontend/src/services/persistence/PersistenceService.test.tsx
@@ -0,0 +1,32 @@
+import { PersistenceService } from "./PersistenceService";
+
+test("can write to persistence service", () => {
+  const service = new PersistenceService();
+  service.set("test", "test");
+  expect(service.get("test")).toBe("test");
+});
+
+test("returns null if key does not exist", () => {
+  const service = new PersistenceService();
+  expect(service.get("test_null")).toBeNull();
+});
+
+test("fails if key is an integer", () => {
+  const service = new PersistenceService();
+  expect(() => service.set(1 as any, "test")).toThrowError();
+});
+
+test("fails if value is an integer", () => {
+  const service = new PersistenceService();
+  expect(() => service.set("test", 1 as any)).toThrowError();
+});
+
+test("fails if key is an object", () => {
+  const service = new PersistenceService();
+  expect(() => service.set({} as any, "test")).toThrowError();
+});
+
+test("fails if value is an object", () => {
+  const service = new PersistenceService();
+  expect(() => service.set("test", {} as any)).toThrowError();
+});
