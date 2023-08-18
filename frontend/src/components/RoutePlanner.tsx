b805e67c7968e0b431f3a246cec672c3657ada76
Aconex <oliver.czerwinski@web.de>
2023-08-18 16:05:28 +0200

    Adjusted RoutePlanner
    Further adjusted the RoutePlanner and added to the Home.tsx.
    Added automatic removal of unused dependencies to the settings.json.

--- frontend/src/components/RoutePlanner.tsx
+++ frontend/src/components/RoutePlanner.tsx
@@ -1,34 +1,31 @@
+import { IonButton, IonDatetime, IonDatetimeButton, IonInput, IonItem, IonLabel, IonList, IonModal } from "@ionic/react";
 import React from "react";
-import { IonDatetime, IonDatetimeButton, IonModal, IonContent, IonList, IonItem, IonInput, IonButton } from "@ionic/react";
 
-const RouteInputContainer: React.FC = () => (
-    <IonContent>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-            <form name="search_routes" onSubmit={submitInput} aria-label="route_planner_input">
-                <IonList style={{ padding: "2%" }}>
-                    <IonItem style={{ marginBottom: "2%" }}>
-                        {/* Date-Time-Picker, allowing the user to select dates in the present aswell as the future */}
-                        <IonDatetimeButton aria-label="Date and Time" datetime="datetime" />
-                        <IonModal keepContentsMounted={true}>
-                            <IonDatetime name="date_time" id="datetime" min={new Date().toISOString()} />
-                        </IonModal>
-                    </IonItem>
-                    <IonItem style={{ marginBottom: "2%" }}>
-                        {/* Generic Input for Start-Position */}
-                        <IonInput name="start" aria-label="Start position" fill="outline" placeholder="Enter start position" />
-                    </IonItem>
-                    <IonItem style={{ marginBottom: "2%" }}>
-                        {/* Generic Input for Destination */}
-                        <IonInput name="destination" aria-label="Destination" fill="outline" placeholder="Enter destination" />
-                    </IonItem>
-                    <IonItem style={{ marginBottom: "2%" }}>
-                        {/* Generic Button for Submission */}
-                        <IonButton type="submit" size="default">Search routes</IonButton>
-                    </IonItem>
-                </IonList>
-            </form>
-        </div>
-    </IonContent>
+const RoutePlanner: React.FC = () => (
+    <>
+        <form name="search_routes" onSubmit={submitInput} aria-label="route_planner_input">
+            <IonList inset={true}>
+                <IonItem lines="inset">
+                    {/* Date-Time-Picker, allowing the user to select dates in the present aswell as the future */}
+                    <IonLabel>Date and Time</IonLabel>
+                    <IonDatetimeButton aria-label="Date and Time" datetime="datetime" />
+                    <IonModal keepContentsMounted={true}>
+                        <IonDatetime name="date_time" id="datetime" min={new Date().toISOString()} />
+                    </IonModal>
+                </IonItem>
+                <IonItem>
+                    {/* Generic Input for Start-Position */}
+                    <IonInput name="start" label="Start position" labelPlacement="floating" placeholder="Enter start position" />
+                </IonItem>
+                <IonItem>
+                    {/* Generic Input for Destination */}
+                    <IonInput name="destination" label="Destination" labelPlacement="floating" placeholder="Enter destination" />
+                </IonItem>
+                <IonButton type="submit" size="default" expand="block">Search routes</IonButton>
+            </IonList>
+        </form>
+    </>
+
 );
 
 {/* TODO later: As discussed in the design structure meeting, an input field is needed, which opens up a modal on focus, where the user can
@@ -39,4 +36,4 @@ function submitInput(): void {
     {/* Empty dummy function to do something after submitting all inputs. */ }
 }
 
-export default RouteInputContainer;
+export default RoutePlanner;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import { IonContent, IonFab, IonFabButton, IonFabList, IonIcon, IonPage } from "@ionic/react";
 import { analytics, menu, person } from "ionicons/icons";
 import LeafletMapContainer from "../components/LeafletMapContainer";
+import "./Home.css";
+import RoutePlanner from "../components/RoutePlanner";
+
 
 const Home: React.FC = () => (
   <IonPage>
     <IonContent fullscreen>
-      <LeafletMapContainer />
+      <RoutePlanner />
       <IonFab slot="fixed" vertical="top" horizontal="end">
         <IonFabButton color="primary">
           <IonIcon icon={menu} />
