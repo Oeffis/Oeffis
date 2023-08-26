40a258c8d7a60c91646854d9f2b0cb0fb599ac3c
Tim Vahlbrock <tim@vahlbrock.de>
2023-08-26 21:45:16 +0200

    add new tests for routePlanner

---
+++ e2e/features/routePlanner/routePlanner.feature
@@ -0,0 +1,12 @@
+Feature: RoutePlanner
+  Background:
+    Given the start page is open
+
+  Scenario: a start location can be picked
+    When the start input is clicked
+    And the mock server is prepared to return a canned response for "Gelsenkirchen Hbf"
+    And "Gelsenkirchen Hbf" is entered into the search field
+    Then one of the results is "Gelsenkirchen, Hbf"
+    And one of the results is "Gelsenkirchen, Hauptbahnhof Parkhaus"
+    When the result "Gelsenkirchen, Hbf" is clicked
+    Then the start location should be "Gelsenkirchen, Hbf"
---
+++ e2e/features/routePlanner/routePlanner.ts
@@ -0,0 +1,30 @@
+import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("the start page is open", () => {
+  cy.visit("/");
+});
+
+When("the start input is clicked", () => {
+  cy.findByTestId("origin-input-clickable").click();
+});
+
+When("the mock server is prepared to return a canned response for {string}", (query: string) => {
+  cy.mocksSetCollection(`Query for '${query}'`);
+});
+
+When("{string} is entered into the search field", (query: string) => {
+  cy.findByTestId("origin-input-search-input").type(query);
+});
+
+Then("one of the results is {string}", (result: string) => {
+  cy.findAllByTestId("locationName").contains(result).should("exist");
+});
+
+When("the result {string} is clicked", (result: string) => {
+  console.log("clicking", result);
+  cy.findAllByTestId("locationName").contains(result).click();
+});
+
+Then("the start location should be {string}", (result: string) => {
+  cy.findByTestId("origin-input-clickable").should("have.text", "Origin: " + result);
+});
