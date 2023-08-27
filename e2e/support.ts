248c6128804fa2d68feb5e0ab54300ddaf0c2a40
Tim Vahlbrock <tim@vahlbrock.de>
2023-08-27 11:55:17 +0200

    try workaround for cypress-testing-library bug

--- e2e/support.ts
+++ e2e/support.ts
@@ -2,3 +2,14 @@ import { register } from "@mocks-server/cypress-commands";
 import "./commands";
 
 register();
+before(() => {
+  cy.configureCypressTestingLibrary({
+    getElementError(message, container) {
+      const error = new Error(
+        [message, container.tagName].filter(Boolean).join("\n\n"),
+      );
+      error.name = "TestingLibraryElementError";
+      return error;
+    },
+  });
+});
