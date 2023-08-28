1b08687406f6bf855388f0cd9357172250416fc4
Dominik Krakau <dominik.krakau@outlook.de>
2023-08-28 15:39:10 +0200

    Fix journey test

--- backend/src/journey/controller/journey.controller.spec.ts
+++ backend/src/journey/controller/journey.controller.spec.ts
@@ -18,25 +18,82 @@ beforeEach(async () => {
 });
 
 it("should query trip", async () => {
-  const mockAlternatives = {
-    journeys: [{
-      legs: [{
-        origin: {
-          name: "Hamburg Hbf",
-          id: "de:02000:10051",
-          latitude: 53.552,
-          longitude: 10.006
-        },
-        destination: {
-          name: "Berlin Hbf",
-          id: "de:11011:8089",
-          latitude: 52.525,
-          longitude: 13.369
-        },
-        name: "ICE 123"
-      }]
-    }]
-  };
+  const mockAlternatives = [
+    {
+      "legs": [
+        {
+          "origin": {
+            "id": "de:05513:5613:91:7",
+            "name": "Gelsenkirchen Hbf",
+            "type": "platform",
+            "details": {
+              "shortName": "7",
+              "parent": {
+                "id": "de:05513:5613",
+                "name": "Gelsenkirchen Hbf",
+                "type": "stop",
+                "details": {
+                  "parent": {
+                    "id": "placeID:5513000:9",
+                    "name": "Gelsenkirchen",
+                    "type": "locality",
+                    "details": {}
+                  },
+                  "latitude": 5288900,
+                  "longitude": 790614
+                }
+              },
+              "latitude": 5288910,
+              "longitude": 790718
+            },
+            "arrival": {},
+            "departure": {
+              "timeEstimated": "2023-08-28T12:50:00Z",
+              "timePlanned": "2023-08-28T12:50:00Z"
+            }
+          },
+          "destination": {
+            "id": "de:05916:7590:91:5",
+            "name": "Wanne-Eickel Hbf",
+            "type": "platform",
+            "details": {
+              "shortName": "2",
+              "parent": {
+                "id": "de:05916:7590",
+                "name": "Wanne-Eickel Hbf",
+                "type": "stop",
+                "details": {
+                  "parent": {
+                    "id": "placeID:5916000:5",
+                    "name": "Herne",
+                    "type": "locality",
+                    "details": {}
+                  },
+                  "latitude": 5284190,
+                  "longitude": 797691
+                }
+              },
+              "latitude": 5284181,
+              "longitude": 797752
+            },
+            "arrival": {
+              "timeEstimated": "2023-08-28T12:54:00Z",
+              "timePlanned": "2023-08-28T12:54:00Z"
+            },
+            "departure": {}
+          },
+          "transportation": {
+            "name": "Regionalzug RE2",
+            "trips": []
+          },
+          "details": {
+            "duration": 240
+          }
+        }
+      ]
+    }
+  ];
+
   vi.spyOn(app.get(JourneyService), "queryJourney").mockResolvedValue(mockAlternatives);
   const requestBody = {
     "originId": "de:02000:100513",
