6df752d5301311c3266767dae7058eeb4aa9d3eb
Paul Heidenreich <paul@paul-h.de>
2023-08-30 15:20:01 +0200

    Also delete users backend folder

--- backend/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { UsersController } from "./controller/users.controller";
-import { UsersService } from "./service/users.service";
-
-@Module({
-  controllers: [UsersController],
-  providers: [UsersService]
-})
-export class UsersModule { }
