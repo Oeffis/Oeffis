bfc7d097c8f642f01b6f9492af82b46ed666bf1b
Paul Heidenreich <paul@paul-h.de>
2023-08-31 14:43:21 +0200

    Switch to new generated api

---
+++ frontend/src/services/apiClients/ApiClientsContext.tsx
@@ -0,0 +1,47 @@
+
+import { createContext, useContext } from "react";
+import { Configuration, JourneyApi, LocationFinderApi } from "../../api";
+import { useAppConfig } from "../config/AppConfigContext";
+
+export type ApiClients = {
+  journeyApi: JourneyApi;
+  locationFinderApi: LocationFinderApi;
+};
+
+export const ApiClientsContext = createContext<ApiClients | null>(null);
+
+export function ApiClientsProvider(props: { children: React.ReactNode }): JSX.Element {
+  const { apiUrl } = useAppConfig();
+
+  const configuration = new Configuration({ basePath: apiUrl });
+
+  const apiClients: ApiClients = {
+    journeyApi: new JourneyApi(configuration),
+    locationFinderApi: new LocationFinderApi(configuration)
+  };
+
+  return (
+    <ApiClientsContext.Provider value={apiClients}>
+      {props.children}
+    </ApiClientsContext.Provider>
+  );
+}
+
+export function useApiClients(): ApiClients {
+  const context = useContext(ApiClientsContext);
+  if (!context) {
+    throw new Error("useApiClients must be used within a ApiClientsProvider");
+  }
+
+  return context;
+}
+
+export function useJourneyApi(): JourneyApi {
+  const { journeyApi } = useApiClients();
+  return journeyApi;
+}
+
+export function useLocationFinderApi(): LocationFinderApi {
+  const { locationFinderApi } = useApiClients();
+  return locationFinderApi;
+}
--- frontend/src/services/config/AppConfigContext.tsx
+++ frontend/src/services/config/AppConfigContext.tsx
@@ -1,10 +1,9 @@
 
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import useSWR from "swr";
-import { OpenAPI } from "../../api";
 import { AppConfig } from "./appConfig";
 
-export const AppConfigContext = createContext<AppConfig>({} as AppConfig);
+export const AppConfigContext = createContext<AppConfig | null>(null);
 
 async function fetcher<C>(url: string): Promise<C> {
   const res = await fetch(url);
@@ -20,11 +19,21 @@ export function AppConfigProvider(props: { children: React.ReactNode }): JSX.Ele
   if (!data) {
     return <div>Loading config...</div>;
   }
-  console.debug("appConfig", data);
-  OpenAPI.BASE = data.apiUrl;
+
+  console.debug("appConfig", { data });
+
   return (
     <AppConfigContext.Provider value={data}>
       {props.children}
     </AppConfigContext.Provider>
   );
+}
+
+export function useAppConfig(): AppConfig {
+  const context = useContext(AppConfigContext);
+  if (!context) {
+    throw new Error("useAppConfig must be used within a AppConfigProvider");
+  }
+
+  return context;
 }
