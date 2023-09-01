d7b81966aa2dc8b861312fa884fb588afc97bcf7
Paul Heidenreich <paul@paul-h.de>
2023-09-01 13:57:33 +0200

    Regenerate api client

---
+++ frontend/src/api/models/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BadRequest';
+export * from './Journey';
+export * from './JourneyLocation';
+export * from './JourneyRequestDto';
+export * from './Leg';
+export * from './LegDetails';
+export * from './LegInfo';
+export * from './Location';
+export * from './LocationCoordinatesDto';
+export * from './LocationDetails';
+export * from './LocationNameDto';
+export * from './Time';
+export * from './Transportation';
+export * from './Trip';
