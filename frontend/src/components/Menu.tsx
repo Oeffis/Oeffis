730f50ad059f4d2895e7fa9780aa764dea9af36a
Dominik Krakau <dominik.krakau@outlook.de>
2023-09-21 14:12:29 +0200

    Add buttons to side menu

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,14 +1,26 @@
-import { IonContent, IonHeader, IonMenu, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonContent, IonHeader, IonItem, IonList, IonMenu, IonTitle, IonToolbar } from "@ionic/react";
 import React from "react";
 
 const Menu: React.FC = () => (
   <IonMenu contentId="main-content">
     <IonHeader>
       <IonToolbar>
-        <IonTitle>Settings</IonTitle>
+        <IonTitle>Menu</IonTitle>
       </IonToolbar>
     </IonHeader>
-    <IonContent className="ion-padding">This is the menu content.</IonContent>
+    <IonContent className="ion-padding">
+      <IonList>
+        <IonItem>
+          <IonButton type="submit" size="default" expand="block">Favorites</IonButton>
+        </IonItem>
+        <IonItem>
+          <IonButton type="submit" size="default" expand="block">User History</IonButton>
+        </IonItem>
+        <IonItem>
+          <IonButton type="submit" size="default" expand="block">User Preferences</IonButton>
+        </IonItem>
+      </IonList>
+    </IonContent>
   </IonMenu>
 );
 
