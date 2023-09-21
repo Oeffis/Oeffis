c2242a54c5736f27eb437126ac8b347403af208c
Dominik Krakau <dominik.krakau@outlook.de>
2023-09-21 14:16:01 +0200

    Add lines and markers to map

--- frontend/src/components/LeafletMapContainer.tsx
+++ frontend/src/components/LeafletMapContainer.tsx
@@ -1,28 +1,62 @@
 import { IonButton } from "@ionic/react";
+import { LatLngBoundsLiteral, LatLngExpression, LatLngTuple } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { ReactElement, useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Polygon, Popup, TileLayer } from "react-leaflet";
 import { Location } from "../api";
 import "./LeafletMapContainer.css";
+import { MapController } from "./MapController";
 
-export type LocationSearchListProps = {
-  locations: Location[];
-  onItemClicked: (location: Location) => void;
+
+export type MapProps = {
+  currentLocation: Location,
+  locations: Location[],
+  showLines: boolean,
+  onItemClicked?: (location: Location) => void
 };
 
-const LeafletMapContainer = ({ locations, onItemClicked }: LocationSearchListProps): JSX.Element => {
-  const [center, setCenter] = useState<[number, number]>([51.5743061234828, 7.0272808779967315]);
+const LeafletMapContainer = ({ currentLocation, locations, showLines, onItemClicked }: MapProps): JSX.Element => {
+
+  const [center, setCenter] = useState<LatLngTuple>([currentLocation.details.latitude, currentLocation.details.longitude]);
   const [zoom, setZoom] = useState<number>();
+  //const [bounds, setBounds] = useState<LatLngBoundsLiteral>([]);
+
+  const getBounds = (): LatLngBoundsLiteral => {
+
+    const bounds: LatLngTuple[] = [];
+
+    if (locations.length !== 0) {
+      locations.map((location) => {
+        bounds.push([location.details.latitude, location.details.longitude]);
+      });
+    } else {
+      bounds.push(center);
+    }
+
+    return bounds;
+  };
 
   const renderMarker = (): ReactElement[] => {
 
     const marker: ReactElement[] = [];
 
-    locations.map(location => {
+    if (currentLocation !== undefined) {
       marker.push(
-        <Marker key={"m" + location.id} position={[location.details.latitude, location.details.longitude]}>
-          <Popup key={"p" + location.id} className="popup">
-            <IonButton key={"ib" + location.id} onClick={() => onItemClicked(location)}>{location.details.shortName}</IonButton>
+        <Marker key={"currentPosition-m"} position={[currentLocation.details.latitude, currentLocation.details.longitude]}>
+          <Popup key={"currentPosition-p"} className="popup">
+            <IonButton key={"currentPosition-ib"}>{"You"}</IonButton>
+          </Popup>
+        </Marker>
+      );
+    }
+
+    locations.map((location, index) => {
+      marker.push(
+        <Marker key={"marker" + index} position={[location.details.latitude, location.details.longitude]}>
+          <Popup key={"popup" + index} className="popup">
+            {onItemClicked !== undefined
+              ? <IonButton key={"btn" + index} onClick={() => onItemClicked(location)}>{location.details.shortName}</IonButton>
+              : <IonButton key={"btn" + index}>{location.details.shortName}</IonButton>}
           </Popup>
         </Marker>
       );
@@ -31,27 +65,23 @@ const LeafletMapContainer = ({ locations, onItemClicked }: LocationSearchListPro
     return marker;
   };
 
-  const calculateCenter = (): [number, number] => {
-
-    let result: [number, number] = [51.5743061234828, 7.0272808779967315];
-
-    if (locations.length !== 0) {
-      let latitude = 0;
-      let longitude = 0;
-      locations.map(location => {
-        latitude = latitude + location.details.latitude;
-        longitude = longitude + location.details.longitude;
-      });
-
-      result = [latitude / locations.length, longitude / locations.length];
-    }
-
-    return result;
+  const getPolygonPositions = (): LatLngExpression[] => {
+    const positions: LatLngExpression[] = [];
+    locations.map(location => {
+      positions.push([location.details.latitude, location.details.longitude]);
+    });
+    return positions;
   };
 
   useEffect(() => {
-    setCenter(calculateCenter());
+
     setZoom(15);
+
+    console.log("Center: " + center);
+    console.log("CurrentLocation: " + currentLocation);
+    console.log("Locations: ");
+    console.log(locations);
+
   }, []);
 
   return zoom && center.length > 0 ? (
@@ -60,7 +90,12 @@ const LeafletMapContainer = ({ locations, onItemClicked }: LocationSearchListPro
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <MapController bounds={getBounds()} />
       {renderMarker()}
+      {showLines
+        ? <Polygon color={"blue"} dashArray={"20,15"} weight={2} positions={getPolygonPositions()} />
+        : <></>}
+
     </MapContainer>
   ) : <div />;
 };
