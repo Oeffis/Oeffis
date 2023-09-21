36773c8efff08f1e70a95b7997eccb0ef8ac25ee
Dominik Krakau <dominik.krakau@outlook.de>
2023-09-21 14:17:02 +0200

    Integrate map and routeplanner into journeypage

--- frontend/src/pages/JourneyPage.tsx
+++ frontend/src/pages/JourneyPage.tsx
@@ -1,39 +1,81 @@
+import { Geolocation } from "@capacitor/geolocation";
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Location } from "../api";
 import LeafletMapContainer from "../components/LeafletMapContainer";
 import Menu from "../components/Menu";
 import RoutePlanner from "../components/RoutePlanner";
 import "./JourneyPage.css";
 
-const JourneyPage: React.FC = () => (
+const JourneyPage: React.FC = () => {
 
-  <IonPage>
-    <IonContent fullscreen>
-      <Menu />
-      <IonPage id="main-content">
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonMenuButton />
-            </IonButtons>
-            <IonTitle>Oeffies</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className="journeyContent">
-          <IonContent className="map">
-            <LeafletMapContainer
-              locations={[]}
-              onItemClicked={(): void => { console.log(""); }}
-            />
-          </IonContent>
-          <IonContent className="planner">
-            <RoutePlanner />
-          </IonContent>
-        </IonContent>
-      </IonPage>
-    </IonContent>
-  </IonPage>
+  const [currentLocation, setCurrentLocation] = useState<Location>();
+  const [origin, setOrigin] = useState<Location>();
+  const [destination, setDestination] = useState<Location>();
 
-);
+  const getLocations = (): Location[] => {
+
+    const locations: Location[] = [];
+
+    if (origin !== undefined) {
+      locations.push(origin);
+    }
+    if (destination !== undefined) {
+      locations.push(destination);
+    }
+
+    return locations;
+  };
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition()
+      .then(position =>
+        setCurrentLocation({
+          id: "",
+          name: "You",
+          type: "locality",
+          details: {
+            shortName: "You",
+            matchQuality: 0,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          }
+        })
+      )
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <Menu />
+        <IonPage id="main-content">
+          <IonHeader>
+            <IonToolbar>
+              <IonButtons slot="start">
+                <IonMenuButton />
+              </IonButtons>
+              <IonTitle>Oeffies</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          {currentLocation !== undefined
+            ? <IonContent className="journeyContent">
+              <IonContent className="map">
+                <LeafletMapContainer
+                  currentLocation={currentLocation}
+                  locations={getLocations()}
+                  showLines={true}
+                />
+              </IonContent>
+              <IonContent className="planner">
+                <RoutePlanner currentLocation={currentLocation} setSelectedOriginLocation={setOrigin} setSelectedDestinationLocation={setDestination} />
+              </IonContent>
+            </IonContent>
+            : <></>}
+        </IonPage>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default JourneyPage;
