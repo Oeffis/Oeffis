77890449c6a7d380db235d213c5bb586f9960c5d
Tim Vahlbrock <tim@vahlbrock.de>
2023-09-22 12:57:03 +0200

    fix recording_time, planned and estimated not saving time

--- schema/create_table_historic_data.sql
+++ schema/create_table_historic_data.sql
@@ -2,10 +2,10 @@ CREATE TABLE historic_data (
     id SERIAL NOT NULL,
     trip_id VARCHAR(20) NOT NULL,
     stop_id VARCHAR(20) NOT NULL,
-    recording_time DATE NOT NULL,
+    recording_time TIMESTAMP NOT NULL,
     is_departure BOOLEAN NOT NULL,
-    planned DATE NOT NULL,
-    estimated DATE,
+    planned TIMESTAMP NOT NULL,
+    estimated TIMESTAMP,
     raw_data VARCHAR NOT NULL,
     PRIMARY KEY (id)
 );
