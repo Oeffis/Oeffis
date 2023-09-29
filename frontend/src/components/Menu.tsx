988430356e97739ee4bc195c9c9bfce8f25b58f1
Dominik Krakau <dominik.krakau@outlook.de>
2023-09-29 16:41:21 +0200

    Add buttons to menu

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,5 +1,7 @@
-import { IonButton, IonContent, IonHeader, IonItem, IonList, IonMenu, IonTitle, IonToolbar } from "@ionic/react";
+import { IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonTitle, IonToolbar } from "@ionic/react";
+import { clipboard, compass, settings, starHalf } from "ionicons/icons";
 import React from "react";
+import "./Menu.css";
 
 const Menu: React.FC = () => (
   <IonMenu contentId="main-content">
@@ -10,15 +12,30 @@ const Menu: React.FC = () => (
     </IonHeader>
     <IonContent className="ion-padding">
       <IonList>
-        <IonItem>
-          <IonButton type="submit" size="default" expand="block">Favorites</IonButton>
-        </IonItem>
-        <IonItem>
-          <IonButton type="submit" size="default" expand="block">User History</IonButton>
-        </IonItem>
-        <IonItem>
-          <IonButton type="submit" size="default" expand="block">User Preferences</IonButton>
-        </IonItem>
+        <IonMenuToggle autoHide={false}>
+          <IonItem button routerLink={"/journey"} routerDirection="none">
+            <IonIcon icon={compass} />
+            <IonLabel>Journey</IonLabel>
+          </IonItem>
+        </IonMenuToggle>
+        <IonMenuToggle autoHide={false}>
+          <IonItem button routerLink={"/favorites"} routerDirection="none">
+            <IonIcon icon={starHalf} />
+            <IonLabel>Favourites</IonLabel>
+          </IonItem>
+        </IonMenuToggle>
+        <IonMenuToggle autoHide={false}>
+          <IonItem button routerLink={"/userHistory"} routerDirection="none">
+            <IonIcon icon={clipboard} />
+            <IonLabel>User History</IonLabel>
+          </IonItem>
+        </IonMenuToggle>
+        <IonMenuToggle autoHide={false}>
+          <IonItem button routerLink={"/userPreferences"} routerDirection="none">
+            <IonIcon icon={settings} />
+            <IonLabel>User Preferences</IonLabel>
+          </IonItem>
+        </IonMenuToggle>
       </IonList>
     </IonContent>
   </IonMenu>
