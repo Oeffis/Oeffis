012dc56a6100397a0fd0702e15e60389a8deb3e2
Tim Vahlbrock <tim@vahlbrock.de>
2023-10-28 17:00:29 +0200

    add typeorm connection to postgres db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { JourneyModule } from "./journey/journey.module";
 import { LocationFinderModule } from "./locationFinder/locationFinder.module";
 import { VrrModule } from "./vrr/vrr.module";
 
 @Module({
-  imports: [LocationFinderModule, JourneyModule, VrrModule],
+  imports: [
+    LocationFinderModule,
+    JourneyModule,
+    VrrModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "postgres",
+      database: "postgres",
+      entities: []
+    })
+  ],
   controllers: [],
   providers: []
 })
