0064792ccb79a603f6873e0eee74e42edd006982
Daniel Kojok <daniel.kojok@brace-enterprise.de>
2023-10-31 13:02:52 +0100

    backend linting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as fs from 'fs';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as fs from "fs";
+import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -12,15 +12,15 @@ async function bootstrap(): Promise<void> {
   //app.useGlobalPipes(new ValidationPipe({ transform: true, disableErrorMessages: true }));
 
   const config = new DocumentBuilder()
-    .setTitle('NestJS Swagger')
-    .setDescription('API description')
-    .setVersion('1.0')
-    .addServer('http://127.0.0.1:3000')
+    .setTitle("NestJS Swagger")
+    .setDescription("API description")
+    .setVersion("1.0")
+    .addServer("http://127.0.0.1:3000")
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  fs.writeFileSync('./swagger-spec.json', JSON.stringify(document, null, 2));
-  SwaggerModule.setup('api-docs', app, document);
+  fs.writeFileSync("./swagger-spec.json", JSON.stringify(document, null, 2));
+  SwaggerModule.setup("api-docs", app, document);
 
   // Enabled for testing purpose
   app.enableCors();
