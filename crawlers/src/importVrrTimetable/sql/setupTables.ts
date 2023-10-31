9906562aaa1de4f14874f18cf53c4164145a8672
Paul Heidenreich <paul@paul-h.de>
2023-10-31 10:00:41 +0100

    Expand gtfs import to not override but add a new version to the database (#391)
    * Expand gtfs import to not override but add a new version to the database
    
    * Add 'vrr_timetable_version_id' to historic_data table
    
    * When crawling delays always use latest version of gtfs,  When writing delays include version of gtfs in record
    
    Closes #350

--- crawlers/src/importVrrTimetable/sql/setupTables.ts
+++ crawlers/src/importVrrTimetable/sql/setupTables.ts
@@ -6,14 +6,33 @@ export async function setupTables(options: ImportVrrTimetablesOptions, tableSche
   const { withPgConnection, folder } = options;
 
   const dropTableSql = await buildDropTableSchemaSql(tableSchema);
-  await withPgConnection(async (pgClient) => {
-    await pgClient.query(dropTableSql);
-  });
-
   const [createTableSql, headerNames] = await buildCreateTableSchemaSql(folder, tableSchema);
-  await withPgConnection(async (pgClient) => {
-    await pgClient.query(createTableSql);
-  });
+
+  if (options.dropTables) {
+    await withPgConnection(async (pgClient) => {
+      await pgClient.query(dropTableSql);
+    });
+
+    await withPgConnection(async (pgClient) => {
+      await pgClient.query(createTableSql);
+    });
+
+  } else {
+    console.log(`Checking if table ${tableSchema.name} is compatible...`);
+
+    // check if schema is compatible
+    const currentHeaderNames = await withPgConnection(async (pgClient) => {
+      const { fields } = await pgClient.query("SELECT * FROM " + tableSchema.name + " LIMIT 0");
+      return fields.map((field) => field.name);
+    });
+
+    const missingColumns = headerNames.filter((header) => !currentHeaderNames.includes(header));
+    if (missingColumns.length > 0) {
+      throw new Error(`Table ${tableSchema.name} is missing columns: ${missingColumns.join(", ")}`);
+    }
+
+    console.log(`Table existing ${tableSchema.name} is compatible.`);
+  }
 
   function buildInsertSql(headerNames: string[]): string {
     const columns = headerNames.map((header) => `"${header}"`).join(", ");
