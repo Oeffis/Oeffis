99d3e6908b132e3f24a6d659ef4ec1f935b84f01
Tim Vahlbrock <tim@vahlbrock.de>
2023-11-02 22:45:45 +0100

    Update frontend/src/components/map/LeafletMapContainer.tsx
    Co-authored-by: Paul Heidenreich <paul@paul-h.de>

--- frontend/src/components/map/LeafletMapContainer.tsx
+++ frontend/src/components/map/LeafletMapContainer.tsx
@@ -55,7 +55,7 @@ const LeafletMapContainer = ({ origin, destination, locationIds, showLines, onIt
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
     {renderMarker()}
-    {<CurrentLocationMapMarker />}
+    <CurrentLocationMapMarker />
     {showLines &&
       <Polygon color={"rgb(77, 77, 77)"} opacity={1} dashArray={"20,15"} weight={2} positions={getPolygonPositions()} />}
   </ReactiveMapContainer>;
