f652d079576e56e36a029cc1666cc5a28b8e8f51
Daniel Kojok <daniel.kojok@brace-enterprise.de>
2023-11-02 13:02:29 +0100

    - progress as locale variable - calculate traveledTimes with date-fns function

--- frontend/src/components/StepProgressComponent.tsx
+++ frontend/src/components/StepProgressComponent.tsx
@@ -1,5 +1,5 @@
 import { IonLabel } from "@ionic/react";
-import { format, minutesToMilliseconds } from "date-fns";
+import { differenceInMilliseconds, format, minutesToMilliseconds } from "date-fns";
 import { IJourneyStep } from "../interfaces/IJourneyStep.interface";
 import "./StepProgressComponent.css";
 
@@ -7,7 +7,6 @@ const formatDateTime = (date: Date): string => format(date, "HH:mm");
 
 export interface StepProgressProps { step: IJourneyStep }
 
-let progress = 0;
 const StepProgressComponent: React.FC<StepProgressProps> = (props: StepProgressProps) => (
   <div className="container" data-testid="journey-step" >
     <div className="left">
@@ -40,10 +39,10 @@ export interface FillLineProps { startTime: Date, travelDurationInMinutes: numbe
 
 export function FillLine(props: FillLineProps): JSX.Element {
 
-  const traveledTimeInMs = new Date().getTime() - props.startTime.getTime();
+  const traveledTimeInMs = differenceInMilliseconds(new Date().getTime(), props.startTime.getTime());
   const travelDurationInMs = minutesToMilliseconds(props.travelDurationInMinutes);
 
-  progress = traveledTimeInMs <= 0 ? 0 : progress = (traveledTimeInMs / travelDurationInMs) * 100;
+  let progress = traveledTimeInMs <= 0 ? 0 : (traveledTimeInMs / travelDurationInMs) * 100;
 
   if (progress > 100) {
     progress = 100;
