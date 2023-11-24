4f9706f02e946ade958e9cea437629c762135e4e
Jan Jakobus <jan.jakobus@studmail.w-hs.de>
2023-11-24 19:29:29 +0100

    Removes journeyDemo page from application (#452)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,35 @@
 import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import { Redirect, Route } from "react-router-dom";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
-
-/* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
 import "@ionic/react/css/display.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/float-elements.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/react/css/normalize.css";
 import "@ionic/react/css/padding.css";
+import "@ionic/react/css/structure.css";
 import "@ionic/react/css/text-alignment.css";
 import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/typography.css";
+import { Redirect, Route } from "react-router-dom";
 import Menu from "./components/Menu";
 import { CurrentLocationProvider } from "./hooks/useCurrentLocation";
 import FavoritesPage from "./pages/FavoritesPage";
 import JourneyPage from "./pages/JourneyPage";
-import JourneysPage from "./pages/JourneysPage";
 import UserHistoryPage from "./pages/UserHistoryPage";
 import UserPreferencesPage from "./pages/UserPreferencesPage";
 import { ApiClientsProvider } from "./services/apiClients/ApiClientsContext";
 import { AppConfigProvider } from "./services/config/AppConfigContext";
-import { FavoriteLocationsProvider, FavoriteRoutesProvider, FavoriteTripsProvider } from "./services/favorites/FavoritesContext";
+import {
+  FavoriteLocationsProvider,
+  FavoriteRoutesProvider,
+  FavoriteTripsProvider
+} from "./services/favorites/FavoritesContext";
 import { PersistenceProvider } from "./services/persistence/PersistenceContext";
 import "./theme/variables.css";
 
@@ -58,9 +61,6 @@ const App: React.FC = () => (
                       </Route>
                       <Route exact path="/userPreferences">
                         <UserPreferencesPage />
-                      </Route>
-                      <Route exact path="/journeyDemo">
-                        <JourneysPage />
                       </Route>
                     </IonRouterOutlet>
                   </IonReactRouter>
