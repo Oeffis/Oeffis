fb726448225e0ccb79336164564350560052f2f7
Daniel Kojok <daniel.kojok@brace-enterprise.de>
2023-11-28 11:14:33 +0100

    fixed build errors

--- frontend/src/components/ResultRoutes/ResultRoutes.tsx
+++ frontend/src/components/ResultRoutes/ResultRoutes.tsx
@@ -7,14 +7,11 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { Swiper as SwiperReact, SwiperSlide } from "swiper/react";
 import logo from "../../../public/images/train_image.png";
-import { FootpathLeg, LegOriginLocationTypeEnum, Location, TransportationLeg } from "../../api";
-import { useJourneyQuery } from "../../hooks/useJourneyQuery";
 import { useLocationByIdOrNull } from "../../hooks/useLocationByIdOrNull";
 import { useStateParams } from "../../hooks/useStateParams";
 import { IJourney } from "../../interfaces/IJourney.interface";
-import { IJourneyStep } from "../../interfaces/IJourneyStep.interface";
 import JourneyDetail from "../JourneyDetail";
-import JourneyListComponent from "../JourneyListComponent";
+import { TripOptionsDisplay } from "../RoutePlanner/TripOptionsDisplay";
 import "./ResultRoutes.css";
 
 const ResultRoutes: React.FC = () => {
@@ -84,7 +81,7 @@ const ResultRoutes: React.FC = () => {
           spaceBetween={100}
           slidesPerView={1}
           initialSlide={0}
-          onInit={(swiper) => setSwiper(swiper)}
+          onInit={(swiper: Swiper) => setSwiper(swiper)}
         >
           <SwiperSlide>
             {
@@ -109,54 +106,3 @@ const ResultRoutes: React.FC = () => {
 };
 
 export default ResultRoutes;
-
-export function TripOptionsDisplay(props: {
-  origin: Location,
-  destination: Location,
-  departure: Date,
-  setJourney: (journey: IJourney) => void
-}): JSX.Element {
-  const { origin, destination, departure } = props;
-
-  // TODO Add user input if datetime should be interpreted as arrival time.
-  const result = useJourneyQuery(origin, destination, departure, false);
-
-  const iJourneys: false | IJourney[] = result.type === "success"
-    && result.journeyResults
-      .map((journey): IJourney => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const legs: (TransportationLeg | FootpathLeg)[] = journey.legs;
-
-        const lastLeg = legs[legs.length - 1];
-        const firstLeg = legs[0];
-
-        return {
-          startStation: firstLeg.origin.name,
-          startTime: firstLeg.origin.departureTimeEstimated,
-          arrivalStation: lastLeg.destination.name,
-          arrivalTime: lastLeg.destination.arrivalTimeEstimated,
-          stops: legs.map((leg): IJourneyStep => ({
-            arrivalTime: leg.destination.arrivalTimeEstimated,
-            startTime: leg.origin.departureTimeEstimated,
-            stationName: leg.origin.name,
-            track: leg.origin.type === LegOriginLocationTypeEnum.Platform
-              ? leg.origin.details.shortName
-              : "",
-            stopName: leg.destination.name,
-            travelDurationInMinutes: leg.details.duration / 60,
-            line: "transportation" in leg ? leg.transportation.line : ""
-          })),
-          travelDurationInMinutes: legs.reduce((acc, leg) => acc + leg.details.duration, 0) / 60
-        };
-      });
-
-  return (
-    <>
-      {result.type === "error" && <div>Error: {result.error.message}</div>}
-      {result.type === "pending" && <div>Searching...</div>}
-      {iJourneys &&
-        <JourneyListComponent setActiveJourney={props.setJourney} journeys={iJourneys} />
-      }
-    </>
-  );
-}
