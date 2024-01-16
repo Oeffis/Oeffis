504f5b3c3d014148dfe18c87ca3b09aa86e0b121
Aconex <66433136+AconexOfficial@users.noreply.github.com>
2024-01-16 13:55:16 +0100

    Added new logo and changed placeholder to logo (#511)
    * Added new logo and changed placeholder to logo
    
    Added new logo and changed placeholder to logo
    
    * Added new logo to Favories, User History and User Preferences
    
    Added new logo to Favories, User History and User Preferences

--- frontend/src/pages/UserPreferencesPage.tsx
+++ frontend/src/pages/UserPreferencesPage.tsx
@@ -1,5 +1,5 @@
 import { IonButtons, IonContent, IonHeader, IonImg, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import logo from "../../public/images/train_image.png";
+import logo from "../../public/images/OeffisLogo1.svg";
 import "./UserPreferencesPage.css";
 
 const UserPreferencesPage: React.FC = () => (
