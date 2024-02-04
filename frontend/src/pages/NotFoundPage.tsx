c152a0c7178f1732a3854bba43b3c669915cf5c4
Dominik Krakau <dominik.krakau@outlook.de>
2024-02-04 19:00:39 +0100

    Create css module for NotFoundPage

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,16 +1,17 @@
 import { IonContent, IonIcon, IonPage } from "@ionic/react";
 import { thunderstorm } from "ionicons/icons";
 import { Header } from "../components/Header";
+import styles from "./NotFoundPage.module.css";
 
 const NotFoundPage: React.FC = () => (
   <IonPage id="main-content">
     <Header />
     <IonContent fullscreen>
-      <div className="error">
-        <IonIcon className="errorIcon" src={thunderstorm} />
+      <div className={styles.error}>
+        <IonIcon className={styles.errorIcon} src={thunderstorm} />
         <div>
-          <p className="errorCode">404</p>
-          <p className="errorMessage">Page not found</p>
+          <p className={styles.errorCode}>404</p>
+          <p className={styles.errorMessage}>Page not found</p>
         </div>
       </div>
     </IonContent>
